f10ff12718e03c725eeb27a2f2ab3ebcd2e82441
keesouwoudba <keesouwoudba@gmail.com>
2026-05-01 09:34:17 +0500

    Add notification bar and shell methods: updateHeader, setContent, show/hide notifications

--- src/com/university/shopping/view/layout/AppShell.java
+++ src/com/university/shopping/view/layout/AppShell.java
@@ -2,6 +2,7 @@ package com.university.shopping.view.layout;
 
 import com.university.shopping.view.components.FooterComponent;
 import com.university.shopping.view.components.HeaderNavComponent;
+import com.university.shopping.view.components.NotificationBarComponent;
 import com.university.shopping.view.contracts.ScreenContext;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -9,19 +10,25 @@ import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 
+import javafx.scene.layout.Pane;
+
 public class AppShell {
     private final BorderPane root;
     private final HeaderNavComponent header;
+    private final NotificationBarComponent notificationBar;
     private final ScrollPane scrollRoot;
     private final VBox pageFlow;
     private final FooterComponent footer;
+    private final VBox topContainer;
 
     public AppShell() {
         this.root = new BorderPane();
         this.header = new HeaderNavComponent();
+        this.notificationBar = new NotificationBarComponent();
         this.scrollRoot = new ScrollPane();
         this.pageFlow = new VBox();
         this.footer = new FooterComponent();
+        this.topContainer = new VBox();
 
         scrollRoot.setContent(pageFlow);
         scrollRoot.setFitToWidth(true);
@@ -29,12 +36,35 @@ public class AppShell {
     }
 
     public Parent buildRoot(ScreenContext context) {
-        root.setTop(header.render(context));
+        topContainer.getChildren().clear();
+        topContainer.getChildren().addAll(header.render(context), notificationBar.getNode());
+        root.setTop(topContainer);
         return root;
     }
 
+    public void updateHeader(ScreenContext context) {
+        // refresh header state and re-render header node
+        header.refresh(context);
+        if (!topContainer.getChildren().isEmpty()) {
+            topContainer.getChildren().set(0, header.render(context));
+        }
+    }
+
+    public void setContent(Node pageNode) {
+        setPageContent(pageNode);
+    }
+
+    public void showNotification(String message, boolean isError) {
+        if (isError) notificationBar.showError(message);
+        else notificationBar.showSuccess(message);
+    }
+
+    public void hideNotification() {
+        notificationBar.clear();
+    }
+
     public void setPageContent(Node pageNode) {
-        pageFlow.getChildren().clear();
+        pageFlow.getChildren().clear(); //always remove previous content
         if (pageNode != null) {
             pageFlow.getChildren().add(pageNode);
         }
