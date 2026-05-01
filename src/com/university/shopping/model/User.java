36aca6ba988ebb32ae8ba92540baedec0d32a922
keesouwoudba <keesouwoudba@gmail.com>
2026-05-01 09:44:53 +0500

    Add Role enum and getRole() to User

--- src/com/university/shopping/model/User.java
+++ src/com/university/shopping/model/User.java
@@ -7,6 +7,11 @@ public class User {
     private boolean isAdmin;
     private final String createdDate;
 
+    public enum Role {
+        ADMIN,
+        CUSTOMER
+    }
+
     public User(String username, String password, boolean isAdmin, String createdDate) {
         this.id = MockDatabase.nextUserId;
         MockDatabase.nextUserId++;
@@ -46,6 +51,10 @@ public class User {
 
     public String getCreatedDate() {
         return createdDate;
+    }
+
+    public Role getRole() {
+        return this.isAdmin ? Role.ADMIN : Role.CUSTOMER;
     }
 
     // setters
